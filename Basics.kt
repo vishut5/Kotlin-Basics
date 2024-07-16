faf6895e1f1fe2d40ec997279930936a9a0c369b
vishut5 <tyagivishu277@gmail.com>
2024-07-16 11:48:46 +0530

    Final commit

---
+++ Basics.kt
@@ -0,0 +1,15 @@
+package `Android 14 Course kotlin fundamentals`
+
+fun main() {
+
+
+    var age = 20
+
+if (age == 20){
+    println("person is mature")
+
+}
+    else {
+    println("person is infant")
+}
+}
